f0eb27379281c5e425f0dbd7c674a5415b8acbda
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-03-20 12:31:51 +0100

    inserite in maniera brutale le immagini (da sistemare più avanti)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 //milestone 1
 
-var apiBaseUrl = 'https://api.themoviedb.org/3';
 
-var source = $("#card-template").html();
+
+var source = $("#card-template").html();         //clono template
 var template = Handlebars.compile(source);
 
-$('#btn-search').click(function(){
+$('#btn-search').click(cerca);                  //richiamo la funzione cerca al click del bottone
+$('#ricerca-input').keypress(function() {
+    if(event.key == 'Enter') {
+        cerca();
+    }
+});
+
+
+/* funzioni */
+
+
+function cerca() {                                       //funzione per ricerca dei titoli dei film
+    var apiBaseUrl = 'https://api.themoviedb.org/3';
+    var posterUrl = "https://image.tmdb.org/t/p";          
     var ricerca = $('#ricerca-input').val();
-    $('#ricerca-input').val('');
+    $('#ricerca-input').val('');                        //questo mi serve per ripulire l'input
     $.ajax({
         url: apiBaseUrl + '/search/movie',
         data: {
@@ -18,25 +31,24 @@ $('#btn-search').click(function(){
         method: 'GET',
         success: function (data) {
             var films = data.results;
+            $('.cards-container').empty();
             for (var i = 0; i < films.length; i++) {
                 var film = films[i];
                 console.log(film);
                 var oggettoFilm = {
                         titolo: film.title,
                         titoloOriginale: film.original_title,
-                        lingua: film.original_language,
-                        voto: film.vote_average
+                        linguaOriginale: film.original_language,
+                        voto: film.vote_average,
+                        poster: posterUrl + "/w342/" + film.poster_path
                 };
-                var caratteristicheFilm = template(oggettoFilm);
+                var caratteristicheFilm = template(oggettoFilm);        //popolo il template
                 $('.cards-container').append(caratteristicheFilm);
 
             }
-
-
         },
         error: function () {
             alert('errore');
         }
     })
-
-});
+}
